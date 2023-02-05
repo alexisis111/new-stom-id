599d7caa6f11e2b55e8b747472e9a9e6353089ba
alexisis111 <shuragilyow@yandex.ru>
2023-02-06 01:23:45 +0300

    настроил tailwind

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.bg}>
+        Hello
+        <div className={styles.gg}>Hello</div>
+    </div>
+
+  );
+}
+
+
+export default App;
