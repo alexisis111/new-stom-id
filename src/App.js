4cad30e3447ed2e88edc9dc9ba774dfda5c18c85
alexisis111 <shuragilyow@yandex.ru>
2023-02-06 01:29:48 +0300

    готов начинать проект

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className={styles.bg}>
-        Hello
-        <div className={styles.gg}>Hello</div>
-    </div>
+    <div className={styles.bg}>Helloow</div>
 
   );
 }
